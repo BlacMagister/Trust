26ffa50d01066794d3198cb4fbcb2728d5d3ca5e
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 00:27:07 +0700

    Update routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify
-from src.blockchain.chain import Blockchain  # Import Blockchain class
+from flask_cors import CORS
+from src.blockchain.chain import Blockchain
 
 app = Flask(__name__)
-blockchain = Blockchain() # Inisialisasi blockchain
+CORS(app) # Enable CORS
+
+blockchain = Blockchain()
 
 @app.route('/chain', methods=['GET'])
 def get_chain():
     """Mengembalikan seluruh rantai blockchain."""
     chain_data = []
     for block in blockchain.chain:
-        chain_data.append(block.__dict__)  # Konversi ke dictionary
+        chain_data.append({
+            "index": block.index,
+            "timestamp": block.timestamp,
+            "transactions": block.transactions,
+            "previous_hash": block.previous_hash,
+            "hash": block.hash
+        })
     return jsonify({"length": len(chain_data),
                     "chain": chain_data})
 
-# Route-rute lain bisa ditambahkan di sini (misalnya, untuk nambah transaksi)
+@app.route('/', methods=['GET'])
+def index():
+    return "ChainKopi API is running!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5002)

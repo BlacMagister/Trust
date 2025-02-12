07dacf074d8f6028330e70ba93043b84f1dc8dca
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 01:27:53 +0700

    Update chain.py

--- src/blockchain/chain.py
+++ src/blockchain/chain.py
@@ -46,16 +46,31 @@ class Blockchain:
         block.hash = block.calculate_hash()
         self.chain.append(block)
 
-    def is_chain_valid(self):
+    def is_chain_valid(self, chain=None, blockchain = None):
         """Memvalidasi integritas rantai."""
-        for i in range(1, len(self.chain)):
-            current_block = self.chain[i]
-            previous_block = self.chain[i-1]
+        if chain is None:
+            chain = self.chain
+
+        if blockchain is None:
+            blockchain = self
+        #Validasi genesis block
+        genesis_block = chain[0]
+        if genesis_block.hash != create_genesis_block().hash:
+            return False
+
+        for i in range(1, len(chain)):
+            current_block = chain[i]
+            previous_block = chain[i-1]
 
             if current_block.hash != current_block.calculate_hash():
+                print("Hash block tidak valid")
                 return False
 
             if current_block.previous_hash != previous_block.hash:
+                print("Previous hash tidak valid")
+                return False
+
+            if not blockchain.is_valid_block(current_block,previous_block):
                 return False
 
         return True

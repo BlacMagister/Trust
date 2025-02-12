560fca8c4d8b45c041a5cb965bdce7e15fd564bb
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 01:02:33 +0700

    Update chain.py

--- src/blockchain/chain.py
+++ src/blockchain/chain.py
@@ -1,9 +1,44 @@
 from src.blockchain.block import Block, create_genesis_block
 import time
+from src.blockchain.consensus import is_authorized, validate_block
+import os
+import json
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import secrets
 
 class Blockchain:
     def __init__(self):
         self.chain = [create_genesis_block()]
+        self.unconfirmed_transactions = []
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        """
+        Fungsi buat nambang blok baru.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.chain[-1]
+
+        new_block = Block(
+            index=last_block.index + 1,
+            timestamp=time.time(),
+            transactions=self.unconfirmed_transactions,
+            previous_hash=last_block.hash,
+            nonce=secrets.randbelow(100)  # Angka random buat bukti kerja
+        )
+
+        # Validasi blok
+        if validate_block(new_block, last_block):
+            self.chain.append(new_block)
+            self.unconfirmed_transactions = []
+            return new_block
+        else:
+            return False
 
     def add_block(self, block):
         """Menambahkan blok baru ke rantai."""
@@ -25,16 +60,12 @@ class Blockchain:
 
         return True
 
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def is_valid_block(self, block, last_block):
+        return validate_block(block, last_block)
+
     def __repr__(self):
         return f"Blockchain with {len(self.chain)} blocks. Valid: {self.is_chain_valid()}"
-
-if __name__ == '__main__':
-    blockchain = Blockchain()
-    new_block = Block(1, time.time(), ["transaksi 1", "transaksi 2"], blockchain.chain[-1].hash)
-    blockchain.add_block(new_block)
-
-    new_block_2 = Block(2, time.time(), ["transaksi 3", "transaksi 4"], blockchain.chain[-1].hash)
-    blockchain.add_block(new_block_2)
-    print(blockchain)
-
-    print(f"Blockchain valid? {blockchain.is_chain_valid()}")
